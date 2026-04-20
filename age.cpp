b30401d53a4f7be9685a2f2088380e5e7f048840
seada <seadaseamn718@gmail.com>
2026-04-20 14:10:18 +0300

    exercise c++

---
+++ age.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int age;
+	age = 30;
+	float hight = 1.7;
+	char grade = 'A';
+	bool pass = false;
+	string name = "Abebe";
+	const double pi = 3.14;
+	
+	cout<<"Age: "<<age<<endl;
+	cout<<"Hight: "<<hight<<endl;
+	cout<<"Grade: "<<grade<<endl;
+	cout<<"pass: "<<pass<<endl;
+	cout<<"Name: "<<name<<endl;
+	cout<<"pi: "<<pi<<endl;
+	return 0;
+}
---
+++ operators.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<< 10 + 12<<endl;
+	cout<< 10 * 12<<endl;
+	cout<< 10 / 5<<endl;
+	cout<< 10 - 5<<endl;
+	cout<< 10 % 8<<endl;
+	
+	int a =15;
+	int b=30;
+	
+	int c=50, d=15;
+	
+	cout<<"a+b= "<<a+b<<endl;
+	cout<<"c+d= "<<c+d<<endl;
+	return 0;
+}
